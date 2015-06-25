5f40fab5acbb906a3aee6eea7130e3fab24a449c
Kawsik Roy <roykawsik@gmail.com>
2015-06-24 22:24:47 -0400

    a

--- biggan01/post-page/my-script.js
+++ biggan01/post-page/my-script.js
@@ -1,1 +1,19 @@
-$("#sidebar").stick_in_parent({parent: $("#body-row"), offset_top: 10, offset_bottom: 100});
+    
+
+// $('#sidebar').stick_in_parent({parent:'.container'});
+
+
+// $("#sidebar").stick_in_parent({parent: $("#body-row"), offset_top: 10, offset_bottom: 100});
+
+
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+
+
+
+
+
+
